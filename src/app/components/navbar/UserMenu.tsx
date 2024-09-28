19dc7e19abe384149f951c2d11d236a9ba06fc2e
Junaid <junaiddshaukat@gmail.com>
2024-09-28 17:10:19 +0500

    Add MenuItem component to the navbar

--- src/app/components/navbar/UserMenu.tsx
+++ src/app/components/navbar/UserMenu.tsx
@@ -2,8 +2,18 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+// useCallback hook helps to freez function or memorize function when tne components is re rendred
+  const toogleOpen = useCallback(()=>{
+    setIsOpen((value) =>  !value)
+  },[])
+
+
   return (
     <div
       className="
@@ -15,7 +25,7 @@ const UserMenu = () => {
         <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer" >
 Travey your home
         </div>
-        <div onClick={()=>{}} className="p-4 md:py-2 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+        <div onClick={toogleOpen} className="p-4 md:py-2 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
 
             <AiOutlineMenu/>
             <div className="hidden md:block">
@@ -24,6 +34,30 @@ Travey your home
 
         </div>
       </div>
+      {
+        isOpen && (
+          <div className="
+          absolute
+          rounded-xl
+          shadow-md
+          w-[40vw]
+          md:w-3/4
+          bg-white
+          overflow-hidden
+          right-0
+          top-12
+          text-sm
+          ">
+            <div className="flex flex-col cursor-pointer">
+              <>
+             <MenuItem onClick={()=>{}} label="Login" /> 
+             <MenuItem onClick={()=>{}} label="Sign Up" /> 
+              </>
+            </div>
+
+          </div>
+        )
+      }
     </div>
   );
 };
